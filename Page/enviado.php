b4f2bd8e3068c102b08c90fe7b0b5ffe8b4e57d8
AxelArani-code <axelaranibar3@gmail.com>
2020-09-21 14:46:37 -0300

    Modelo

---
+++ Page/enviado.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enviado</title>
+</head>
+<body>
+    <img src="../assets/img/Icons/Enviado.png" style="position: absolute;" alt="">
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<lottie-player src="https://assets10.lottiefiles.com/packages/lf20_tW99ax.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop autoplay></lottie-player>
+</body>
+</html> 
